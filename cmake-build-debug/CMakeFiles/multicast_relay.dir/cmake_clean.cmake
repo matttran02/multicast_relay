1ffb67eff8d162b325d31466aaabcf88a9597472
mattt <matttran02@gmail.com>
2022-07-19 13:01:19 -0400

    "remove more warnings"

---
+++ cmake-build-debug/CMakeFiles/multicast_relay.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multicast_relay.dir/client.c.obj"
+  "CMakeFiles/multicast_relay.dir/server.c.obj"
+  "libmulticast_relay.dll.a"
+  "multicast_relay.exe"
+  "multicast_relay.exe.manifest"
+  "multicast_relay.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/multicast_relay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
